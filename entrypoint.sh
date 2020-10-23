0342401202ab395737d6df34f9f6ae2f1278fd5f
Jhow Rodrigues <developer.jhow@gmail.com>
2020-10-23 00:10:38 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo 'starting file execution'
 
 dir="cd $2"
-init="eb init $5 --profile=$3 --region=$4 --quiet && eb deploy"
+init="eb init $5 --profile=$3 --region=$4 --quiet --debug && eb deploy --debug"
 
 
 $dir
