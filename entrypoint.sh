bf48b8bf94256a80a6ef4fb8dd0fabdc2a72fc2d
Jhow Rodrigues <developer.jhow@gmail.com>
2020-10-23 00:06:43 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -l
 
-echo 'starting deploy'
+echo 'starting file execution'
 
 dir="cd $2"
-init="eb init $5 --profile=$3 --region=$4 --quiet"
-command="eb $1"
+init="eb init $5 --profile=$3 --region=$4 --quiet && eb deploy"
+
 
 $dir
+ls
+
+echo 'starting init and deploy'
 $init
-$command
